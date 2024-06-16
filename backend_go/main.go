9f3bbf51c73abbb01accbe46a13556c6353bce54
tpiechowiak <szuedat06@gmail.com>
2024-06-16 16:02:07 +0200

    Created first base endpoints in new backend

--- backend_go/main.go
+++ backend_go/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"zsk.poznan.pl/backend/handlers/date"
+	"zsk.poznan.pl/backend/handlers/subs"
 	"zsk.poznan.pl/backend/handlers/teacher"
 )
 
@@ -13,10 +14,9 @@ func main() {
 	{
 		v1.GET("teacher", teacher.GetTeachers)
 		v1.GET("teacher/:name", teacher.GetTeacherByName)
-		v1.GET("teacher/all", teacher.GetAllSubs)
+		v1.GET("teacher/all", subs.GetAllSubs)
 		v1.GET("date", date.GetDate)
 	}
 
-	// :8080 - default port if no PORT environment variable was defined.
 	r.Run()
 }
